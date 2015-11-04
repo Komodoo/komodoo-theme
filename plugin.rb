76bce3fc39437ae78d2a0cfd6e86468e3aa1a4f4
Swizz540 <gerodel.quentin@gmail.com>
2015-11-04 15:59:34 +0100

    All done

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: komodoo-theme
+# about: Komodoo - Forum RP
+# authors: Quentin Gerodel
+
+# Special thanks to SitePoint team
+# Based on : https://github.com/sitepoint/discourse-theme
+
+register_asset "stylesheets/common/foundation/colors.scss", :variables
+
+register_asset "stylesheets/common/components/badges.css.scss"
+register_asset "stylesheets/common/base/topic-list.scss"
+
+register_asset "stylesheets/desktop/topic.scss", :desktop
