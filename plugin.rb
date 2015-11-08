8d285236df6c49f436f3a374b53f979a4054eea7
Swizz540 <gerodel.quentin@gmail.com>
2015-11-08 14:53:55 +0100

    Clear radius for real

--- plugin.rb
+++ plugin.rb
@@ -7,13 +7,8 @@
 
  register_asset "stylesheets/common/foundation/colors.scss", :variables
 
-register_asset "stylesheets/common/components/badges.css.scss"
-
 register_asset "stylesheets/common/base/header.scss"
-<<<<<<< HEAD
 register_asset "stylesheets/common/base/discourse.scss"
-=======
 register_asset "stylesheets/common/base/topic-list.scss"
->>>>>>> parent of e9c0cac... Clear radius
 
 register_asset "stylesheets/desktop/topic.scss", :desktop
