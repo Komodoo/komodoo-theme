e9c0cac58c3f45128a4b89674641562143c39e0a
Swizz540 <gerodel.quentin@gmail.com>
2015-11-08 10:21:41 +0100

    Clear radius

--- plugin.rb
+++ plugin.rb
@@ -5,10 +5,9 @@
 # Special thanks to SitePoint team
 # Based on : https://github.com/sitepoint/discourse-theme
 
- register_asset "stylesheets/common/foundation/colors.scss", :variables
+register_asset "stylesheets/common/foundation/colors.scss", :variables
 
 register_asset "stylesheets/common/components/badges.css.scss"
 register_asset "stylesheets/common/base/header.scss"
-register_asset "stylesheets/common/base/topic-list.scss"
 
 register_asset "stylesheets/desktop/topic.scss", :desktop
