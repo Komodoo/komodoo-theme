7422f44b2c8c4caf124aa68045fbdd5cf7a0fe36
Swizz540 <gerodel.quentin@gmail.com>
2015-11-08 14:52:56 +0100

    Revert "Clear radius"
    This reverts commit e9c0cac58c3f45128a4b89674641562143c39e0a.

--- plugin.rb
+++ plugin.rb
@@ -5,11 +5,15 @@
 # Special thanks to SitePoint team
 # Based on : https://github.com/sitepoint/discourse-theme
 
-register_asset "stylesheets/common/foundation/colors.scss", :variables
+ register_asset "stylesheets/common/foundation/colors.scss", :variables
 
 register_asset "stylesheets/common/components/badges.css.scss"
 
 register_asset "stylesheets/common/base/header.scss"
+<<<<<<< HEAD
 register_asset "stylesheets/common/base/discourse.scss"
+=======
+register_asset "stylesheets/common/base/topic-list.scss"
+>>>>>>> parent of e9c0cac... Clear radius
 
 register_asset "stylesheets/desktop/topic.scss", :desktop
